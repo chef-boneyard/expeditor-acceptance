8abefbb91c1cf661b3a2633d7dbfe53ae1c9850a
Nathaniel Kierpiec <nate.kierpiec@gmail.com>
2021-09-01 12:50:11 -0400

    Update and rename create-another-pr.sh to songs.sh

---
+++ .expeditor/songs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eou pipefail
+
+# Run the command which will modify some files
+echo "this is the song that never ends. it goes on and on my friends. some people started singing not knowing what it was. and they'll continue singing it forever just because..." >> songs.txt
+
